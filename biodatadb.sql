4f5264b69fa017e29374382015603ef5bd47032a
DzakyFirdaus96 <110155168+DzakyFirdaus96@users.noreply.github.com>
2022-08-11 17:00:48 +0700

    Add files via upload

---
+++ biodatadb.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `biodata` (
+  `nama` varchar(30) NOT NULL,
+  `kelas` varchar(30) DEFAULT NULL,
+  `jurusan` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`nama`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
